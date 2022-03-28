3482196d8acd7439e70cc379814f237b24e9d14a
pavel.tokarev <pavel.tokarev@mail.ntc51>
2022-03-28 17:16:21 +0300

    add tabs  задел на будущее

--- ui_mywidget.h
+++ ui_mywidget.h
@@ -18,6 +18,7 @@
 #include <QtWidgets/QProgressBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
 #include <QtWidgets/QTableWidget>
 #include <QtWidgets/QWidget>
 
@@ -28,9 +29,14 @@ class Ui_MyWidget
 public:
     QWidget *centralwidget;
     QGridLayout *gridLayout;
+    QTabWidget *m_tabWidget;
+    QWidget *m_tab;
+    QGridLayout *gridLayout_2;
     QTableWidget *m_table;
     QProgressBar *m_pBar;
     QPushButton *m_button;
+    QWidget *m_tab_2;
+    QGridLayout *gridLayout_3;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -38,12 +44,26 @@ public:
     {
         if (MyWidget->objectName().isEmpty())
             MyWidget->setObjectName(QString::fromUtf8("MyWidget"));
-        MyWidget->resize(570, 400);
+        MyWidget->resize(600, 400);
         centralwidget = new QWidget(MyWidget);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         gridLayout = new QGridLayout(centralwidget);
         gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
-        m_table = new QTableWidget(centralwidget);
+        m_tabWidget = new QTabWidget(centralwidget);
+        m_tabWidget->setObjectName(QString::fromUtf8("m_tabWidget"));
+        m_tabWidget->setEnabled(true);
+        m_tabWidget->setStyleSheet(QString::fromUtf8("QTabBar::tab { height: 30px;}"));
+        m_tab = new QWidget();
+        m_tab->setObjectName(QString::fromUtf8("m_tab"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(m_tab->sizePolicy().hasHeightForWidth());
+        m_tab->setSizePolicy(sizePolicy);
+        gridLayout_2 = new QGridLayout(m_tab);
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        gridLayout_2->setContentsMargins(9, 9, -1, 9);
+        m_table = new QTableWidget(m_tab);
         if (m_table->columnCount() < 5)
             m_table->setColumnCount(5);
         QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
@@ -61,29 +81,41 @@ public:
         m_table->horizontalHeader()->setDefaultSectionSize(100);
         m_table->verticalHeader()->setDefaultSectionSize(23);
 
-        gridLayout->addWidget(m_table, 0, 0, 1, 2);
+        gridLayout_2->addWidget(m_table, 0, 0, 1, 2);
 
-        m_pBar = new QProgressBar(centralwidget);
+        m_pBar = new QProgressBar(m_tab);
         m_pBar->setObjectName(QString::fromUtf8("m_pBar"));
         m_pBar->setValue(0);
 
-        gridLayout->addWidget(m_pBar, 1, 0, 1, 1);
+        gridLayout_2->addWidget(m_pBar, 1, 0, 1, 1);
 
-        m_button = new QPushButton(centralwidget);
+        m_button = new QPushButton(m_tab);
         m_button->setObjectName(QString::fromUtf8("m_button"));
 
-        gridLayout->addWidget(m_button, 1, 1, 1, 1);
+        gridLayout_2->addWidget(m_button, 1, 1, 1, 1);
+
+        m_tabWidget->addTab(m_tab, QString());
+        m_tab_2 = new QWidget();
+        m_tab_2->setObjectName(QString::fromUtf8("m_tab_2"));
+        gridLayout_3 = new QGridLayout(m_tab_2);
+        gridLayout_3->setObjectName(QString::fromUtf8("gridLayout_3"));
+        m_tabWidget->addTab(m_tab_2, QString());
+
+        gridLayout->addWidget(m_tabWidget, 0, 0, 1, 1);
 
         MyWidget->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MyWidget);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 570, 21));
+        menubar->setGeometry(QRect(0, 0, 600, 21));
         MyWidget->setMenuBar(menubar);
         statusbar = new QStatusBar(MyWidget);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
         MyWidget->setStatusBar(statusbar);
 
         retranslateUi(MyWidget);
+
+        m_tabWidget->setCurrentIndex(0);
+
 
         QMetaObject::connectSlotsByName(MyWidget);
     } // setupUi
@@ -107,6 +139,9 @@ public:
         ___qtablewidgetitem4->setText(QCoreApplication::translate("MyWidget", "\320\235\320\276\320\274\320\265\321\200 \321\204\321\200\320\265\320\271\320\274\320\260\n"
 "\320\261\320\270\320\272\320\276\320\275\320\260 ", nullptr));
         m_button->setText(QCoreApplication::translate("MyWidget", "\320\236\321\202\320\272\321\200\321\213\321\202\321\214", nullptr));
+        m_tabWidget->setTabText(m_tabWidget->indexOf(m_tab), QCoreApplication::translate("MyWidget", "\320\220\320\275\320\260\320\273\320\270\320\267 \n"
+"\320\277\321\200\320\276\320\277\321\203\321\211\320\265\320\275\320\275\321\213\321\205 \320\277\320\260\320\272\320\265\321\202\320\276\320\262", nullptr));
+        m_tabWidget->setTabText(m_tabWidget->indexOf(m_tab_2), QCoreApplication::translate("MyWidget", "Tab 2", nullptr));
     } // retranslateUi
 
 };
